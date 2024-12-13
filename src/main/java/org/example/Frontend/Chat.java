53d2124499e4fe002a60ac306fd837a5eafaa5fa
oguzcannn <117283308+oguzcannn@users.noreply.github.com>
2024-12-13 14:54:02 +0300

    Mesajlaşma fonksiyonunun temelleri düzelitldi. Arayüze eklendi.

--- src/main/java/org/example/Frontend/Chat.java
+++ src/main/java/org/example/Frontend/Chat.java
@@ -3,6 +3,8 @@ package org.example.Frontend;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Chat extends JFrame {
     private JTextField messagesInput;
@@ -36,9 +38,18 @@ public class Chat extends JFrame {
 
             }
         });
+
+
+        messagesInput.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = messagesInput.getText();
+                if (!message.isEmpty()){
+                    listModel.addElement(message);
+                    messagesList.setModel(listModel);
+                    messagesInput.setText("");
+                }
+            }
+        });
     }
-
-
-
-
 }
